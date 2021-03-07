aaf273fd9cc138bc7db94d33d2a805da4e895af8
danielkhan-v1 <grigorovich314@gmail.com>
2021-03-07 21:22:40 +0200

    feat: create solution for vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,95 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(option) {
+        this.option = option;
+    }
+
+    alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    square = [];
+
+    generateSquare() {
+        for (let i = 0; i < this.alph.length; i++) {
+            let row = this.alph.slice(i);
+            row += this.alph.slice(0, i);
+            this.square.push(row.split(""));
+        }
+    }
+
+    keyExtender(key, message) {
+        let newKey = "";
+        let y = 0;
+
+        for (let i = 0; i < message.length; i++) {
+            if (this.alph.indexOf(message.toUpperCase()[i]) == -1) {
+                newKey += message[i];
+                continue;
+            }
+
+            newKey += key[y];
+            y++;
+            if (y == key.length) y = 0;
+        }
+
+        return newKey;
+    }
+
+    encrypt(message, key) {
+        if (message == undefined || key == undefined) throw new Error("Got ya!");
+
+        let encrypted = "";
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        this.generateSquare();
+        key = this.keyExtender(key, message);
+
+        for (let i = 0; i < message.length; i++) {
+            if (this.alph.indexOf(message[i]) === -1) {
+                encrypted += message[i];
+                continue;
+            }
+
+            let x = this.alph.indexOf(message[i]);
+            let y = this.alph.indexOf(key[i]);
+
+            encrypted += this.square[x][y];
+        }
+
+        if (this.option === false) {
+            return encrypted.split("").reverse().join("");
+        }
+
+        return encrypted;
+    }
+
+    decrypt(message, key) {
+        if (message == undefined || key == undefined) throw new Error("Got ya!");
+
+        let decrypted = "";
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+
+        this.generateSquare();
+        key = this.keyExtender(key, message);
+
+        for (let i = 0; i < message.length; i++) {
+            if (this.alph.indexOf(message[i]) === -1) {
+                decrypted += message[i];
+                continue;
+            }
+            let x = this.alph.indexOf(key[i]);
+            let y = this.square[x].indexOf(message[i]);
+            decrypted += this.alph[y];
+        }
+
+        if (this.option === false) {
+            return decrypted.split("").reverse().join("");
+        }
+
+        return decrypted;
+    }
 }
 
 module.exports = VigenereCipheringMachine;
